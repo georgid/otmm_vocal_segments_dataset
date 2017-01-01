d3ec7b53173dc1539c55e527f2b4ba2f5f8e32b1
georgid <georgi.dzhambazov@upf.edu>
2017-01-01 10:20:49 +0100

    readme edited

--- scripts/modify_beat_anno_timestamps-1.py
+++ scripts/modify_beat_anno_timestamps-1.py
@@ -12,20 +12,22 @@ offset = 4.16
 offset = 0
 fout = NamedTemporaryFile(delete=False)
 URI = '/Users/joro/Documents/Phd/UPF/voxforge/myScripts/turkish_makam_vocal_segments_dataset/data/f5a89c06-d9bc-4425-a8e6-0f44f7c108ef/f5a89c06-d9bc-4425-a8e6-0f44f7c108ef.beats'
+URI = '/Users/joro/Documents/Phd/UPF/voxforge/myScripts/turkish_makam_vocal_segments_dataset/data/92ef6776-09fa-41be-8661-025f9b33be4f/92ef6776-09fa-41be-8661-025f9b33be4f.beats'
+
 URI_target = URI + '_'
 with open(URI,'rb') as fin, fout:
-    w = writer(fout)
+    w = writer(fout, delimiter='\t')
 
-    reader = reader(fin)
+    reader = reader(fin, delimiter=',')
     rows = [r for r in reader]
     
     for row in rows:
             row[0]= float(row[0]) + offset
             w.writerow(row)
+# rename to original file name or to another name
 shutil.move(fout.name, URI_target)            
 
 
-# In[ ]:
 
 
 
